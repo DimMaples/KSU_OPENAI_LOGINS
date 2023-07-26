dbba282acda15bf3fa73058dd07bf36cdd32109c
Dmitriy <dklonov@ksu.ks.ua>
2023-07-26 15:10:00 +0300

    sdgf

--- app.py
+++ app.py
@@ -84,11 +84,6 @@ def login():
 def select_login():
     return render_template("select.html")
 
-@app.route("/main_chat", defaults={"path": "index.html"})
-@app.route("/<path:path>")
-def static_file(path):
-    return app.send_static_file(path)
-
 @app.route("/", defaults={"path": "index.html"})
 @app.route("/<path:path>")
 def static_file(path):
