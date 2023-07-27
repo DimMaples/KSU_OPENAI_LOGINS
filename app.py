77f44d4f849e39b628e0286971a223815658c79c
Dmitriy <dklonov@ksu.ks.ua>
2023-07-27 14:48:08 +0300

    OUTPUTE

--- app.py
+++ app.py
@@ -73,7 +73,9 @@ def login():
 def micro_redirect():
     result = auth.complete_log_in(request.args)
     if "error" in result:
-        return redirect('/select-login')
+        print(result['error'])
+        return 1
+        #return redirect('/select-login')
     session['user']  = 'here'
     session['token'] = 'token'
     return redirect("/")
@@ -87,13 +89,10 @@ def select_login():
 def static_file(path):
 #check session. if has login token then output chat
 #if not output select page
-    print(session)
     if 'user' in session and 'token' in session:
-        #return app.send_static_file(path)
-    #else :     
-    #    return redirect('/select-login')
-        print(session)
-        return 1
+        return app.send_static_file(path)
+    else :     
+        return redirect('/select-login')
 
 # ACS Integration Settings
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE")
