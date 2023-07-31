723967e2683c63f08ad1dc1eed0fc403f00409ad
Dmitriy <dklonov@ksu.ks.ua>
2023-07-31 16:49:44 +0300

    TEST

--- app.py
+++ app.py
@@ -88,11 +88,11 @@ def select_login():
 def static_file(path):
 #check session. if has login token then output chat
 #if not output select page    auth_uri
-    if session.get('auth_uri'):
-        
+    if session.get('user'):
         return app.send_static_file(path)
+        #return render_template('/select.html', result=session)
     else :
-        return render_template('/select.html', result=session["auth_uri"])
+        return render_template('/select.html', result=session.get("_auth_flow"))
 
 # ACS Integration Settings
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE")
