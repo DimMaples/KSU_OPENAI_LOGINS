7a3f6865ebf01dac1c5c1dd4f917a85f1787aff7
admin <dklonov@ksu.ks.ua>
2023-10-20 15:01:10 +0300

    test render template

--- app.py
+++ app.py
@@ -87,8 +87,9 @@ def static_file(path):
     if session.get("_auth_flow") is None:
         return render_template('select.html', result=result)
     else:
-        return render_template('select.html', result=result)
+        #return render_template('select.html', result=result)
         #return app.send_static_file(path)
+        return render_template('index.html', result=result)
 
 # ACS Integration Settings
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE")
