3764797a61665e6641e1a30a4184cc8ef3f7eb09
admin <dklonov@ksu.ks.ua>
2023-10-26 12:37:21 +0300

    test new route

--- app.py
+++ app.py
@@ -64,11 +64,18 @@ def google_auth():
 def login():
     return render_template("login.html", version="1", **auth.log_in(
         ["User.ReadBasic.All"], # Have user consent to scopes during log-in
-        redirect_uri="https://ksu24ai-restore-bf97.azurewebsites.net/.auth/login/aad/callback",
+        redirect_uri="https://ksu24ai.azurewebsites.net/.auth/login/aad/callback",
     ))
 
 @app.route("/.auth/login/aad/callback")
 def micro_redirect():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return redirect('/select-login')
+    return redirect("/")
+
+@app.route(".auth/login/done")
+def micro_login_done():
     result = auth.complete_log_in(request.args)
     if "error" in result:
         return redirect('/select-login')
@@ -83,7 +90,7 @@ def select_login():
 @app.route("/<path:path>")
 def static_file(path):
     result = session
-    session["_auth_flow"] = "aaaaaaaa"
+    #session["_auth_flow"] = "aaaaaaaa"
     if session.get("_auth_flow") is None:
         return render_template('select.html', result=result)
     else:
