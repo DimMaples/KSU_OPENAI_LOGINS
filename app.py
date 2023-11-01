dc9f9a978db10a91488a0f7b1dc1314c708a161d
admin <dklonov@ksu.ks.ua>
2023-11-01 11:39:50 +0200

    test

--- app.py
+++ app.py
@@ -81,10 +81,10 @@ def google_auth():
 def login():
     return render_template("login.html", version="1", **auth.log_in(
         ["User.ReadBasic.All"], # Have user consent to scopes during log-in
-        redirect_uri="https://ksu24ai-restore-bf97.azurewebsites.net/.auth/login/aad/callback",
+        redirect_uri="https://ksu24ai-restore-bf97.azurewebsites.net/.auth/login/aad/callback/",
     ))
 
-@app.route("/.auth/login/aad/callback")
+@app.route("/.auth/login/aad/callback/")
 def micro_redirect():
     result = auth.complete_log_in(request.args)
     session["_auth_flow"] = result
@@ -94,16 +94,16 @@ def micro_redirect():
 
 @app.route("/.auth/login/done/")
 def micro_login_done():
-    #result = auth.complete_log_in(request.args)
+    result = auth.complete_log_in(request.headers)
     #if "error" in result:
     #    return redirect('/select-login')
     #session["_auth_flow"] = "aaaaaaaa"
-    if '_auth_flow' not in session:
-        return render_template('select.html', result = session)
+    #if '_auth_flow' not in session:
+    #    return render_template('select.html', result = session)
     #return current_app.send_static_file("index.html")
     #return redirect(url_for("static_file"))
     #rererewrew
-    return render_template('select.html', result = session)
+    return render_template('select.html', result = result)
    
 @app.route("/select-login")
 def select_login():
